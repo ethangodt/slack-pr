92d4df303d3b234e6098c206f3b25f7086f195e9
Ethan Godt <egodt@paypal.com>
2016-06-02 21:54:02 -0700

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import morgan from 'morgan';
+const app = express();
+
+app.use(morgan('tiny'));
+
+app.use(function (req, res) {
+	res.status(200).send('It\'s working');
+});
+
+app.listen(3000, function () {
+	console.log('App is listening on 3000');
+});
---
+++ bootstrap.js
@@ -0,0 +1,6 @@
+// bootstrap file
+require('babel-register');
+require('babel-polyfill');
+
+// start the app
+require('./app/');
